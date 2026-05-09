8e1495cb81d0edb366df3d73c681bb93a5e82cdd
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2026-05-09 17:34:34 +0200

    Add `tinymist` language server configuration

--- .config/nvim/lua/thinety/config/lsp.lua
+++ .config/nvim/lua/thinety/config/lsp.lua
@@ -59,6 +59,96 @@ vim.lsp.config("rust_analyzer", {
   },
 })
 
+vim.lsp.config("tinymist", {
+  capabilities = {
+    workspace = {
+      didChangeConfiguration = {
+        dynamicRegistration = true,
+      },
+    },
+  },
+  settings = {
+    lint = { enabled = true },
+    preview = {
+      browsing = {
+        args = {
+          "--data-plane-host=127.0.0.1:0",
+          "--open",
+        },
+      },
+    },
+  },
+  on_init = function(client)
+    local export_targets = { "paged", "html" }
+    local preview_modes = { "document", "slide" }
+
+    local function update_settings(settings)
+      client.settings = vim.tbl_deep_extend("force", client.settings, settings)
+      client:notify("workspace/didChangeConfiguration", { settings = {} })
+    end
+
+    local subcommands = {
+      ["set-export-target"] = function(arg)
+        assert(vim.list_contains(export_targets, arg))
+        update_settings({
+          exportTarget = arg,
+        })
+      end,
+      ["set-preview-mode"] = function(arg)
+        assert(vim.list_contains(preview_modes, arg))
+        update_settings({
+          preview = {
+            browsing = {
+              args = vim.list_extend(
+                { "--preview-mode=" .. arg },
+                vim.lsp.config["tinymist"].settings.preview.browsing.args
+              ),
+            },
+          },
+        })
+      end,
+      ["preview"] = function()
+        client:exec_cmd({
+          title = "preview",
+          command = "tinymist.startDefaultPreview",
+        })
+      end,
+    }
+
+    local available_subcommands = vim.tbl_keys(subcommands)
+    local subcommands_complete = {
+      ["set-export-target"] = export_targets,
+      ["set-preview-mode"] = preview_modes,
+    }
+
+    vim.api.nvim_create_user_command(
+      "Tinymist",
+      function(opts)
+        subcommands[opts.fargs[1]](opts.fargs[2])
+      end,
+      {
+        nargs = "+",
+        complete = function(_, line)
+          local split = vim.split(line, "%s+")
+          if #split == 2 then
+            return available_subcommands
+          elseif #split == 3 then
+            return subcommands_complete[split[2]]
+          end
+        end
+      })
+  end,
+  on_attach = function(client, buf)
+    vim.keymap.set("n", "<leader>tp", function()
+      client:exec_cmd({
+        title = "pin",
+        command = "tinymist.pinMain",
+        arguments = { vim.api.nvim_buf_get_name(buf) },
+      })
+    end, { desc = "Tinymist: pin main file", buf = buf })
+  end,
+})
+
 vim.lsp.config("cssls", {
   settings = {
     -- do not warn on tailwind at-rules
