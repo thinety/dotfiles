0d51e733518ca95fde0bd5477031e6766057ef1d
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2026-06-25 18:19:52 +0200

    neovim: fix diagnostic autocmd causing 100% of cpu usage

--- .config/nvim/lua/thinety/config/diagnostic.lua
+++ .config/nvim/lua/thinety/config/diagnostic.lua
@@ -2,17 +2,22 @@ vim.diagnostic.config({
   float = { source = true },
 })
 
+local diagnostic_augroup = "thinety.hide-diagnostics"
+
 vim.keymap.set("n", "<Leader>d", function()
   vim.diagnostic.config({
     virtual_lines = { current_line = true }
   })
-end, { desc = "Show diagnostic messages" })
 
-vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "InsertEnter" }, {
-  group = vim.api.nvim_create_augroup("thinety.hide-diagnostics", { clear = true }),
-  callback = function()
-    vim.diagnostic.config({
-      virtual_lines = false
-    })
-  end,
-})
+  vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "InsertEnter" }, {
+    group = vim.api.nvim_create_augroup(diagnostic_augroup, { clear = true }),
+    callback = function()
+      vim.diagnostic.config({
+        virtual_lines = false
+      })
+
+      -- clear autocmd after running once
+      vim.api.nvim_create_augroup(diagnostic_augroup, { clear = true })
+    end,
+  })
+end, { desc = "Show diagnostic messages" })
